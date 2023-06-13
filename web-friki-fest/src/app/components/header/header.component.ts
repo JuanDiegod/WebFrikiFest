f02a77d9d3c0f5a12a892324ac4038c2491de45b
Juan Diego Diaz <juandia09@hotmail.com>
2023-06-13 12:57:27 -0500

    Avances en el header de la pagina

--- web-friki-fest/src/app/components/header/header.component.ts
+++ web-friki-fest/src/app/components/header/header.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
 
+  private subscription: Subscription = new Subscription();
+  
+  public dateNow = new Date();
+  public dDay = new Date('Aug 12 2023 10:00:00');
+  milliSecondsInASecond = 1000;
+  hoursInADay = 24;
+  minutesInAnHour = 60;
+  SecondsInAMinute  = 60;
+
+  public timeDifference: number = 0;
+  public secondsToDday: number = 0;
+  public minutesToDday: number = 0;
+  public hoursToDday: number = 0;
+  public daysToDday: number = 0;
+
+  private getTimeDifference () {
+    this.timeDifference = this.dDay.getTime() - new  Date().getTime();
+    this.allocateTimeUnits(this.timeDifference);
+  }
+
+  private allocateTimeUnits (timeDifference: number) {
+    this.secondsToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond) % this.SecondsInAMinute);
+    this.minutesToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond * this.minutesInAnHour) % this.SecondsInAMinute);
+    this.hoursToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond * this.minutesInAnHour * this.SecondsInAMinute) % this.hoursInADay);
+    this.daysToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond * this.minutesInAnHour * this.SecondsInAMinute * this.hoursInADay));
+  }
+
+  ngOnInit(): void {
+    this.subscription = interval(1000)
+           .subscribe(x => { this.getTimeDifference(); });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
